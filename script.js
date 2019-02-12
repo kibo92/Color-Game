ef025e6f8f320cbce1d19123940d931035252ef9
Bach <borislukovic@outlook.com>
2019-02-12 03:08:04 +0100

    script.js update
    Adding reset button(play again) and its logic into span div tag

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+    //generate all new colors
+    colors = generateRandomColors(6);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colorDisplay to match picked Color
+    colorDisplay.textContent = pickedColor;
+    //change colors of squares
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+});
 
 //Change span h1 rgb to picked color 
 colorDisplay.textContent = pickedColor;
@@ -19,18 +33,18 @@ for (var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
         if(clickedColor === pickedColor) {
-
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
         } else {
-            
             this.style.backgroundColor = "#232323"
             messageDisplay.textContent = "Try again.";
         }
     });
 }
 
+//change color of all squares when corect one is picked
 function changeColors(color) {
     //loop through all squares
     for(var i = 0; i < squares.length; i++) {
@@ -40,10 +54,12 @@ function changeColors(color) {
 
 };
 
+//picks a random color square as the corect answer
 function pickColor() {
-    var random = Math.floor(Math.random() * colors.length);
+    var random = Math.floor(Math.random() * colors.length); //array index starts with 0 that's why we are ok with 5
     return colors[random];
 };
+
 
 function generateRandomColors(num) {
     //make an array
@@ -56,6 +72,7 @@ function generateRandomColors(num) {
     //return that array
     return arr;
 }
+
 
 function randomColor() {
     //pick a "red" from 0 - 255
