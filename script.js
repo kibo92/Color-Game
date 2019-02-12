c30e17dea8dd97cdbe91f371e117298b73a5330d
Bach <borislukovic@outlook.com>
2019-02-12 16:59:02 +0100

    Adding buttons
    Adding hard and easy buttons and logic for them, plus adding var for keeping track of number of squares

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function() {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function() {
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++) {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+    }
+});
+
 
 resetButton.addEventListener("click", function(){
     //generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     //pick a new random color from array
     pickedColor = pickColor();
     //change colorDisplay to match picked Color
@@ -20,8 +54,10 @@ resetButton.addEventListener("click", function(){
     h1.style.backgroundColor = "#232323";
 });
 
+
 //Change span h1 rgb to picked color 
 colorDisplay.textContent = pickedColor;
+
 
 for (var i = 0; i < squares.length; i++) {
     //add initial colors to squares
@@ -44,6 +80,7 @@ for (var i = 0; i < squares.length; i++) {
     });
 }
 
+
 //change color of all squares when corect one is picked
 function changeColors(color) {
     //loop through all squares
@@ -53,6 +90,7 @@ function changeColors(color) {
     }
 
 };
+
 
 //picks a random color square as the corect answer
 function pickColor() {
